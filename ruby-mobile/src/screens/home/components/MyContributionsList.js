4b7bad464dcf31924467bd7745f4507bba3ac9ed
fify mawia <mawiafreda@gmail.com>
2020-03-17 11:19:12 +0000

    added unverified contributions

--- ruby-mobile/src/screens/home/components/MyContributionsList.js
+++ ruby-mobile/src/screens/home/components/MyContributionsList.js
@@ -6,31 +6,33 @@ import { contributionsScreen } from '../../contributionsScreen';
 
 
 const MyContributionsList = ({ contributions }) => {
+    const renderLastContribution = () => {
+        const lastIndex = contributions.length - 1;
+        const contribution = contributions[lastIndex];
+        return (
+            <View>
+
+                <Card style={{ alignSelf: 'center', marginTop: 5, width: 280, height: 50 }}>
+                <CardItem header button onPress={() => alert("Pending Bank Verification")}>
+                <Text style={{color: '#008000', marginTop: 5, fontSize: 10, }}>
+                    Not Verified
+                </Text>
+                <Text style={{marginTop: 20}}>
+                    {contribution.createdAt}
+                </Text>
+                    </CardItem>
+                </Card>
+
+            </View>
+        );
+    }
   // eslint-disable-next-line no-console
 //   console.log(contributions);
   return (
     <View style={styles.root}>
       <View style={styles.titleContainer}>
       {/* if(contributions.lenghth === i+1) */}
-      {contributions.map((contribution, i) => (
-          <View key={i} >
-
-          <View>
-
-            <Card style={{ alignSelf: 'center', marginTop: 5, width: 280, height: 50 }}>
-            <CardItem header button onPress={() => alert("Pending Bank Verification")}>
-            <Text style={{color: '#008000', marginTop: 5, fontSize: 10, }}>
-                Not Verified
-            </Text>
-            <Text style={{marginTop: 20}}>
-              {contribution.createdAt}
-            </Text>
-              </CardItem>
-          </Card>
-
-          </View>
-        </View>
-      ))}
+      {renderLastContribution()}
 
 
       </View>
